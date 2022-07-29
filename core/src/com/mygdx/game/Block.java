1be0c59bed79f1db28c974a4cde3a5cb5016c8fa
yurifmorais <yurimorais7113@gmail.com>
2022-07-29 20:53:05 -0300

    Atualizacao

--- core/src/com/mygdx/game/Block.java
+++ core/src/com/mygdx/game/Block.java
@@ -3,12 +3,10 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class Block {
-    private int type;
     private int BLOCK_SIZE = 32;
     private TextureRegion region;
 
-    public Block (int type, TextureRegion region) {
-        this.type = type;
+    public Block (TextureRegion region) {
         this.region = region;
     }
     public void render (SpriteBatch batch, int x, int y) {
--- core/src/com/mygdx/game/Plataform.java
+++ core/src/com/mygdx/game/Plataform.java
@@ -4,37 +4,47 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
+import java.util.Hashtable;
+
 public class Plataform{
+    private Hashtable<Integer, Block> mapping;
     private int map[][];
     private int X_SIZE = 32;
     private int Y_SIZE = 16;
     private int TILE_SIZE = 32;
     private Texture baseTexture; // pra mostrar na tela temos que fazer isso
     private TextureRegion region;
-    private Block ground;
-    private Block ground0;
 
     public Plataform () {
         map = new int[X_SIZE][Y_SIZE];
         baseTexture = new Texture(Gdx.files.internal("map.png"));
-        region = new TextureRegion(baseTexture, 0, 0, TILE_SIZE, TILE_SIZE);
-        ground = new Block(1, new TextureRegion(baseTexture, 16, 0, 32, 16));
-        ground0 = new Block(2, new TextureRegion(baseTexture, 16, 16, 32, 32));
-
+        this.mapping = new Hashtable<Integer, Block>();
+        mappingCreate();
         for (int i = 0; i < X_SIZE; i++) {
-            map[i][1] = 1;
+            map[i][1] = 4;
         }
         for (int i = 0; i < X_SIZE; i++) {
-            map[i][0] = 2;
+            map[i][0] = 5;
         }
     }
+    private void mappingCreate(){
+        Integer id = 1;
+        region = new TextureRegion(baseTexture, 0, 0, TILE_SIZE, TILE_SIZE);
+        Block block;
+        for (int x = 0; x < 3; x++){
+            for (int y = 0; y < 3; y++){
+                block = new Block(new TextureRegion(baseTexture, x * 32, y * 32, 32, 32));
+                mapping.put(id, block);
+                id++;
+            }
+        }
+    }
+
     public void render (SpriteBatch batch) {
         for (int x = 0; x < X_SIZE; x++) {
             for (int y = 0; y < Y_SIZE; y++) {
-                if (map[x][y] == 1) {
-                    ground.render(batch, x, y);
-                } else if (map[x][y] == 2) {
-                    ground0.render(batch, x, y);
+                if (map[x][y] != 0){
+                    mapping.get(map[x][y]).render(batch, x, y);
                 }
             }
         }
